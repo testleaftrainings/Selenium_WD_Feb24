4af83f9dac70ce71886b7f640b5a4ef87b294320
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-02-26 14:11:35 +0530

    Add files via upload
    Week1Day1

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	
+	//Type main and press a key ctrl + spacebar
+	//main method is the entry point of a java program.
+	//without main method i can't able to execute.
+	public static void main(String[] args) {
+		
+		// Print --> type syso ctrl + spacebar
+		System.out.println("Welcome to Testleaf :)");
+	}
+	
+}
---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		
+		// Datatype variableName = value
+				// For variablename use camalCase or lowerCase 
+					// lowercase
+					// camelCase
+		// ;(semi-colon) is mandatory java to end statement)
+		byte age = 127;  // -128 to 127
+		short year = 2024;
+		int salary = 30000000;
+		long phno = 8925411174L;
+
+	}
+
+}
